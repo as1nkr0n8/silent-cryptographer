eb386409238bbb1ade863f096203bd15675e876d
as1nkr0n8 <alkeshgamer@gmail.com>
2017-04-15 19:05:30 +0530

    Add files via upload

---
+++ testone.m
@@ -0,0 +1,271 @@
+function varargout = testone(varargin)
+% TESTONE MATLAB code for testone.fig
+%      TESTONE, by itself, creates a new TESTONE or raises the existing
+%      singleton*.
+%
+%      H = TESTONE returns the handle to a new TESTONE or the handle to
+%      the existing singleton*.
+%
+%      TESTONE('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in TESTONE.M with the given input arguments.
+%
+%      TESTONE('Property','Value',...) creates a new TESTONE or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before testone_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to testone_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help testone
+
+% Last Modified by GUIDE v2.5 04-Apr-2017 16:25:37
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @testone_OpeningFcn, ...
+                   'gui_OutputFcn',  @testone_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+% --- Executes just before testone is made visible.
+function testone_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to testone (see VARARGIN)
+
+% Choose default command line output for testone
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes testone wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = testone_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes on button press in browse.
+function browse_Callback(hObject, eventdata, handles)
+% hObject    handle to browse (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+%[filename,pathname,filterindex] = ...
+ %   uigetfile({'*.jpg','*.jpeg','*.bmp'},'Select the image file');
+%above command will select the image file in which message is to be encoded
+%if pathname == 0
+ %   return
+%end   %if no file selected function returns null
+[FileName,PathName] = uigetfile({'*.jpg';'*.jpeg';'*.bmp'},'Select image file to encode');
+
+g=imread(strcat(PathName,FileName));
+global a
+a=g;
+
+
+
+function text_Callback(hObject, eventdata, handles)
+% hObject    handle to text (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of text as text
+%        str2double(get(hObject,'String')) returns contents of text as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function text_CreateFcn(hObject, ~, ~)
+% hObject    handle to text (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in crypt.
+function crypt_Callback(hObject, eventdata, handles)
+% hObject    handle to crypt (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+msg = get(handles.text,'String');
+global a;
+img = a;
+msg_temp = double(msg);     % Converts from ASCII to Integer Values.
+msg_dim = num2str(length(msg_temp));
+msg_length = length(msg_dim);
+z = 0;
+if msg_length < 7
+    padtext = 7 - msg_length;
+    for z = 1:padtext
+        msg_dim = horzcat('0',msg_dim);
+    end
+    msg_head = horzcat('t',msg_dim);
+    % Applying Header To Beginning of Message to be Encoded.
+    msg_temp_head = horzcat(msg_head,msg_temp);
+end
+
+tot_hiding_pix = max(cumprod(size(img)));
+tot_data = max(cumprod(size(msg_temp_head)));
+
+if tot_hiding_pix <= tot_data
+     error('Insufficient Hiding Space in Image')
+end
+
+enc_key=69;
+
+msg_enc = bitxor(uint8(msg_temp_head),uint8(enc_key));
+
+msg_enc_set = dec2bin(msg_enc, 8);
+
+img_prep = im2uint8(img);
+
+
+
+rm = 1; gm = 1; bm = 1;     % Initializing Counters
+rn = 1; gn = 1; bn = 1;
+
+[maxM,maxN] = size(img_prep);
+z = 0;
+
+% RUN_TIME Variable indicates the number of Message "Words" that need to be
+%   encoded in the IMG_PREP "Canvas" Image.
+run_time = length(msg_enc_set);
+
+for z = 1:run_time;
+    temp_code = msg_enc_set(z,:);
+    % Bit 1: Red
+    if str2double(temp_code(1)) == 0
+        img_prep(rm,rn,1) = bitand(img_prep(rm,rn,1),uint8(254));
+    else
+        img_prep(rm,rn,1) = bitor(img_prep(rm,rn,1),uint8(1));
+    end
+    
+    rm = rm + 1;
+    % This next step is used to determine whether or not we have reached
+    %   the end end of the image. We then need to move to the next column
+    %   and reset our pattern to the top row. Since we have no idea when we
+    %   will reach this point we have to check this EVERY time after we
+    %   increase the rm/gm/bm counter.
+    if rm > maxM
+        rn = rn + 1;
+        rm = 1;
+    end
+    % Bit 2: Green
+    if str2double(temp_code(2)) == 0
+        img_prep(gm,gn,2) = bitand(img_prep(gm,gn,2),uint8(254));
+    else
+        img_prep(gm,gn,2) = bitor(img_prep(gm,gn,2),uint8(1));
+    end
+    
+    gm = gm + 1;
+    if gm > maxM
+        gn = gn + 1;
+        gm = 1;
+    end
+    % Bit 3: Blue
+    if str2double(temp_code(3)) == 0
+        img_prep(bm,bn,3) = bitand(img_prep(bm,bn,3),uint8(254));
+    else
+        img_prep(bm,bn,3) = bitor(img_prep(bm,bn,3),uint8(1));
+    end
+    
+    bm = bm + 1;
+    if bm > maxM
+        bn = bn + 1;
+        bm = 1;
+    end
+    % Bit 4: Blue
+    if str2double(temp_code(4)) == 0
+        img_prep(bm,bn,3) = bitand(img_prep(bm,bn,3),uint8(254));
+    else
+        img_prep(bm,bn,3) = bitor(img_prep(bm,bn,3),uint8(1));
+    end
+    
+    bm = bm + 1;
+    if bm > maxM
+        bn = bn + 1;
+        bm = 1;
+    end
+    % Bit 5: Green
+    if str2double(temp_code(5)) == 0
+        img_prep(gm,gn,2) = bitand(img_prep(gm,gn,2),uint8(254));
+    else
+        img_prep(gm,gn,2) = bitor(img_prep(gm,gn,2),uint8(1));
+    end
+    
+    gm = gm + 1;
+    if gm > maxM
+        gn = gn + 1;
+        gm = 1;
+    end
+    % Bit 6: Red
+    if str2double(temp_code(6)) == 0
+        img_prep(rm,rn,1) = bitand(img_prep(rm,rn,1),uint8(254));
+    else
+        img_prep(rm,rn,1) = bitor(img_prep(rm,rn,1),uint8(1));
+    end
+    rm = rm + 1;
+    if rm > maxM
+        rn = rn + 1;
+        rm = 1;
+    end
+    % Bit 7: Red
+    if str2double(temp_code(7)) == 0
+        img_prep(rm,rn,1) = bitand(img_prep(rm,rn,1),uint8(254));
+    else
+        img_prep(rm,rn,1) = bitor(img_prep(rm,rn,1),uint8(1));
+    end
+    rm = rm + 1;
+    if rm > maxM
+        rn = rn + 1;
+        rm = 1;
+    end
+    % Bit 8: Green
+    if str2double(temp_code(8)) == 0
+        img_prep(gm,gn,2) = bitand(img_prep(gm,gn,2),uint8(254));
+    else
+        img_prep(gm,gn,2) = bitor(img_prep(gm,gn,2),uint8(1));
+    end
+    
+    gm = gm + 1;
+    if gm > maxM
+        gn = gn + 1;
+        gm = 1;
+    end
+    
+end
+
+J = img_prep;
+[file,path]=uiputfile('*.bmp','Save encoded image file as');
+imwrite(mat2gray(J),strcat(path,file));
---
+++ testtwo.m
@@ -0,0 +1,323 @@
+function varargout = testtwo(varargin)
+% TESTTWO MATLAB code for testtwo.fig
+%      TESTTWO, by itself, creates a new TESTTWO or raises the existing
+%      singleton*.
+%
+%      H = TESTTWO returns the handle to a new TESTTWO or the handle to
+%      the existing singleton*.
+%
+%      TESTTWO('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in TESTTWO.M with the given input arguments.
+%
+%      TESTTWO('Property','Value',...) creates a new TESTTWO or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before testtwo_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to testtwo_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help testtwo
+
+% Last Modified by GUIDE v2.5 13-Apr-2017 23:53:26
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @testtwo_OpeningFcn, ...
+                   'gui_OutputFcn',  @testtwo_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before testtwo is made visible.
+function testtwo_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to testtwo (see VARARGIN)
+
+% Choose default command line output for testtwo
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes testtwo wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = testtwo_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes on button press in browse.
+function browse_Callback(hObject, eventdata, handles)
+% hObject    handle to browse (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+[FileName,PathName] = uigetfile('*.bmp','Select image file to decode');
+
+g=imread(strcat(PathName,FileName));
+global a
+a=g;
+
+% --- Executes on button press in decrypt.
+function decrypt_Callback(hObject, eventdata, handles)
+% hObject    handle to decrypt (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+global a;
+img = a;
+rm = 1; gm = 1; bm = 1;     % Initializing Counters
+rn = 1; gn = 1; bn = 1;
+
+header = [];
+[maxM, maxN, chan] = size(img);
+
+for z = 1:8;
+    temp = zeros(1,8);
+    % Red    
+    temp(1,1) = mod(img(rm,rn,1),2);    
+    rm = rm + 1;
+    % This next step is used to determine whether or not we have reached
+    %   the end end of the image. We then need to move to the next column
+    %   and reset our pattern to the top row. Since we have no idea when we
+    %   will reach this point we have to check this EVERY time after we
+    %   increase the rm/gm/bm counter.
+    if rm > maxM
+        rn = rn + 1;
+        rm = 1;
+        if rn > maxN
+            break
+        end
+    end
+    % Green
+    temp(1,2) = mod(img(gm,gn,2),2);
+    gm = gm + 1;
+    if gm > maxM
+        gn = gn + 1;
+        gm = 1;
+        if gn > maxN
+            break
+        end
+    end
+    % Blue
+    temp(1,3) = mod(img(bm,bn,3),2);
+    bm = bm + 1;
+    if bm > maxM
+        bn = bn + 1;
+        bm = 1;
+    end
+    % Blue
+    temp(1,4) = mod(img(bm,bn,3),2);
+    bm = bm + 1;
+    if bm > maxM
+        bn = bn + 1;
+        bm = 1;
+    end
+    % Green
+    temp(1,5) = mod(img(gm,gn,2),2);
+    gm = gm + 1;
+    if gm > maxM
+        gn = gn + 1;
+        gm = 1;
+        if gn > maxN
+            break
+        end
+    end
+    % Red    
+    temp(1,6) = mod(img(rm,rn,1),2);    
+    rm = rm + 1;
+    if rm > maxM
+        rn = rn + 1;
+        rm = 1;
+        if rn > maxN
+            break
+        end
+    end
+    % Red    
+    temp(1,7) = mod(img(rm,rn,1),2);    
+    rm = rm + 1;
+    if rm > maxM
+        rn = rn + 1;
+        rm = 1;
+        if rn > maxN
+            break
+        end
+    end
+    % Green
+    temp(1,8) = mod(img(gm,gn,2),2);
+    gm = gm + 1;
+    if gm > maxM
+        gn = gn + 1;
+        gm = 1;
+        if gn > maxN
+            break
+        end
+    end      
+    tempstr = num2str(temp);
+    header = vertcat(header,tempstr);
+end
+
+enc_key = 69;  % Used for Test Phase
+msg_head_set = bin2dec(header);
+temp_head = bitxor(uint8(msg_head_set),uint8(enc_key));
+
+
+
+msg_head_set = bin2dec(header);
+temp_head = bitxor(uint8(msg_head_set),uint8(enc_key));
+
+%CASE 1: Text Set
+dim1 = char(temp_head(2:8));
+m = str2double(dim1);
+n = 1;    
+
+
+
+
+z = 0;
+
+enc_msg = [];
+stopmax = (m * n);
+
+for z = 1:stopmax
+    temp = zeros(1,8);
+    % Red    
+    temp(1,1) = mod(img(rm,rn,1),2);    
+    rm = rm + 1;
+    % This next step is used to determine whether or not we have reached
+    %   the end end of the image. We then need to move to the next column
+    %   and reset our pattern to the top row. Since we have no idea when we
+    %   will reach this point we have to check this EVERY time after we
+    %   increase the rm/gm/bm counter.
+    if rm > maxM
+        rn = rn + 1;
+        rm = 1;
+        if rn > maxN
+            break
+        end
+    end
+    % Green
+    temp(1,2) = mod(img(gm,gn,2),2);
+    gm = gm + 1;
+    if gm > maxM
+        gn = gn + 1;
+        gm = 1;
+        if gn > maxN
+            break
+        end
+    end
+    % Blue
+    temp(1,3) = mod(img(bm,bn,3),2);
+    bm = bm + 1;
+    if bm > maxM
+        bn = bn + 1;
+        bm = 1;
+    end
+    % Blue
+    temp(1,4) = mod(img(bm,bn,3),2);
+    bm = bm + 1;
+    if bm > maxM
+        bn = bn + 1;
+        bm = 1;
+    end
+    % Green
+    temp(1,5) = mod(img(gm,gn,2),2);
+    gm = gm + 1;
+    if gm > maxM
+        gn = gn + 1;
+        gm = 1;
+        if gn > maxN
+            break
+        end
+    end
+    % Red    
+    temp(1,6) = mod(img(rm,rn,1),2);    
+    rm = rm + 1;
+    if rm > maxM
+        rn = rn + 1;
+        rm = 1;
+        if rn > maxN
+            break
+        end
+    end
+    % Red    
+    temp(1,7) = mod(img(rm,rn,1),2);    
+    rm = rm + 1;
+    if rm > maxM
+        rn = rn + 1;
+        rm = 1;
+        if rn > maxN
+            break
+        end
+    end
+    % Green
+    temp(1,8) = mod(img(gm,gn,2),2);
+    gm = gm + 1;
+    if gm > maxM
+        gn = gn + 1;
+        gm = 1;
+        if gn > maxN
+            break
+        end
+    end      
+    tempstr = num2str(temp);
+    enc_msg = vertcat(enc_msg,tempstr);
+end
+
+msg_dec_set = bin2dec(enc_msg);
+msg_dec = bitxor(uint8(msg_dec_set),uint8(enc_key));
+
+msg_set = msg_dec;
+    msg_out = char(msg_set');
+    
+msg = msg_out;
+disp(msg);
+set(handles.op2, 'string', msg);
+
+
+
+function op2_Callback(hObject, eventdata, handles)
+% hObject    handle to op2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of op2 as text
+%        str2double(get(hObject,'String')) returns contents of op2 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function op2_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to op2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
